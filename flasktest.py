50ac97593c28d293cf87f5d37bb9af4be3b2fcc9
JCharante <ralphcharante@gmail.com>
2015-10-23 12:54:19 -0400

    test

--- flasktest.py
+++ flasktest.py
@@ -2,10 +2,13 @@ from flask import Flask
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(4, GPIO.OUT)
 
 app = Flask(__name__)
+
+GPIO.output(4, 0)
 
 @app.route('/')
 def hello_world():
@@ -14,9 +17,9 @@ def hello_world():
 @app.route('/blink')
 def blink():
     print('blinked')
-    GPIO.output(11, True)
-    time.sleep(1)
-    GPIO.output(11, False)
+    GPIO.output(4, 1)
+    time.sleep(5)
+    GPIO.output(4, 0)
     return 'LED Blinked'
 
 if __name__ == '__main__':
